e34226da53deb178d15545d35c07638343271b22
Shane Richardson <srichardson1928@hotmail.co.uk>
2020-08-17 16:07:41 +0100

    added img return funcitonality

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// This will be used to setup timer for data collection
+/*function intervalFunc() {
+    console.log('Cant stop me now!');
+  }
+  
+  setInterval(intervalFunc, 1500);
+*/
+
+// Basic Homepage
 app.get("/", function(req, res){
-    res.render("index")
+    res.render("index");
 });
 
+
+// img retrieval
+app.get("/img/:name", function(req, res){
+
+    res.sendFile('/imgs/' + req.params.name, { root: __dirname});
+})
+
+// Catch all 
 app.get("*", function(req, res){
     res.status(404);
     res.send("Page does not exist");
